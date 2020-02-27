1d460aec78c0b8fbb219a56f81b14f86f77ea685
Eugene Agyeman <eugene.agyeman1@yahoo.co.uk>
2020-02-27 18:48:21 +0000

    Implemented Cost-Per-Click, Cost per Acquisition, Conversions, CPM and Total cost of a campaign in pounds and pence.
    Signed-off-by: Eugene Agyeman <eugene.agyeman1@yahoo.co.uk>

--- src/main/java/DataHandler/DataHandler.java
+++ src/main/java/DataHandler/DataHandler.java
@@ -1,12 +1,12 @@
 package DataHandler;
 
+import DataHandler.pojos.ClickRecord;
 import DataHandler.pojos.ImpressionRecord;
 import DataHandler.pojos.Record;
+import DataHandler.pojos.ServerRecord;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Multimaps;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import static DataHandler.Parser.*;
 import static DataHandler.Parser.clickLogsParser;
@@ -14,9 +14,9 @@ import static DataHandler.Parser.serverLogsParser;
 
 public class DataHandler {
     private static final DataStorage storage = new DataStorage();
-    private static Multimap impressionRecords;
-    private static Multimap clickRecords;
-    private static Multimap serverRecords;
+    private static Multimap<String, ImpressionRecord> impressionRecords;
+    private static Multimap<String, ClickRecord> clickRecords;
+    private static Multimap<String, ServerRecord> serverRecords;
 
     public DataHandler() {
         try {
@@ -39,11 +39,18 @@ public class DataHandler {
             impressionRecords = DataStorage.getImpressionRecords();
             clickRecords = DataStorage.getClickRecords();
             serverRecords = DataStorage.getServerRecords();
-            serverLogsCount();
+
+            System.out.println("Total Cost of Campaign: " + totalCostofCampaigninPence());
+            System.out.println("CPM: " + costPerThousandImpressions());
+            System.out.println("Number of Conversions: " + numberOfConversions());
+            System.out.println("Cost Per Acquisition: " + costPerAcquisition());
+            System.out.println("Cost Per Click: " + costPerClick());
+
+            /*serverLogsCount();
             clickCount();
             impressionsCount();
             impressionsByGender();
-            impressionsByAge();
+            impressionsByAge();*/
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,24 +67,26 @@ public class DataHandler {
         System.out.println("Total Server Records: " + size);
     }
 
-    private void clickCount() {
+    private int clickCount() {
 
         int size = clickRecords.entries().size();
         System.out.println("Total Click Logs: " + size);
+        return size;
     }
 
-    private void impressionsCount() {
+    private int impressionsCount() {
         int size = impressionRecords.entries().size();
         System.out.println("Total Impression Records: " + size);
+        return size;
 
     }
 
     private void impressionsByGender() {
         int impressionsMale = 0;
-        for (Object value : impressionRecords.values()) {
+        for (Record value : impressionRecords.values()) {
             String stringValue = value.toString();
             if (stringValue.contains("Male"))
-                    impressionsMale++;
+                impressionsMale++;
         }
         int impressionsFemale = 0;
         for (Object value : impressionRecords.values()) {
@@ -98,15 +107,15 @@ public class DataHandler {
         for (Object value : impressionRecords.values()) {
             String stringValue = value.toString();
             if (stringValue.contains("<25"))
-                impressionsBelow25 ++;
+                impressionsBelow25++;
             else if (stringValue.contains("25-34"))
-                impressions25To34 ++;
+                impressions25To34++;
             else if (stringValue.contains("35-44"))
-                impressions35To44 ++;
+                impressions35To44++;
             else if (stringValue.contains("45-54"))
-                impressions45To54 ++;
+                impressions45To54++;
             else if (stringValue.contains(">54"))
-                impressionsAbove54 ++;
+                impressionsAbove54++;
         }
         System.out.println("Impressions <25: " + impressionsBelow25);
         System.out.println("Impressions 25-34: " + impressions25To34);
@@ -114,4 +123,45 @@ public class DataHandler {
         System.out.println("Impressions <45-54: " + impressions45To54);
         System.out.println("Impressions >54: " + impressionsAbove54);
     }
+
+    //TODO: Ask about pounds vs pence
+    private Float totalCostofCampaigninPence() {
+        Float totalCost = impressionRecords.values()
+                .stream()
+                .map(ImpressionRecord::getImpressionCost)
+                .reduce((float) 0.00, Float::sum);
+        return totalCost;
+    }
+
+    private Double costOfCampaignInPounds(Float costInPence) {
+        return (double) (costInPence / 100);
+    }
+
+    private Double costPerThousandImpressions() {
+        //Loop through every impression
+        int numOfImpressions = impressionsCount();
+        float totalCost = totalCostofCampaigninPence();
+        return (double) ((1000 * (totalCost)) / numOfImpressions);
+
+    }
+
+    private int numberOfConversions() {
+        int conversions = (int) serverRecords.values()
+                .stream()
+                .map(ServerRecord::getConversion)
+                .filter(response -> response.equalsIgnoreCase("yes"))
+                .count();
+        return conversions;
+    }
+
+    private Double costPerAcquisition() {
+        Double cost = (double) (totalCostofCampaigninPence() / numberOfConversions());
+        return cost;
+    }
+
+    private Double costPerClick() {
+        return (Double) (double) (totalCostofCampaigninPence() / clickCount());
+    }
 }
+
+
