2c80a8eb8ae3ea016f6554470b9afe7f443184e6
Eugene Agyeman <eugene.agyeman1@yahoo.co.uk>
2020-02-28 13:37:43 +0000

    Updated .gitignore file
    Signed-off-by: Eugene Agyeman <eugene.agyeman1@yahoo.co.uk>

---
+++ src/test/java/DataHandler/DataHandlerTest.java
@@ -0,0 +1,12 @@
+package DataHandler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataHandlerTest {
+
+    @Test
+    public void dummy() {
+    }
+}
