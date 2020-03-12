bc922e99902af0558bc89cff0f59e773453de4b6
Emilia <emiliaszynkowska@outlook.comi>
2020-03-12 21:14:18 +0000

    Overview Controller Connected

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class Main extends Application {
     private static Stage mainWindow;
     private DashboardModel model;
+    private OverviewController controller;
 
     public static void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(
@@ -24,17 +25,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        model = new Configuration().buildDashboard();
         mainWindow = stage;
+        controller = new OverviewController();
+        controller.setModel(model);
 
         FXMLLoader loader = new FXMLLoader();
+        loader.setController(controller);
         loader.setLocation(this.getClass().getResource("/fxml/Overview.fxml"));
-
-        model = new Configuration().buildDashboard();
         Scene scene = new Scene(loader.load());
         mainWindow.setScene(scene);
+        controller.initialiseOverview();
 
-        OverviewController controller = loader.getController();
-        controller.initModel(model);
         mainWindow.show();
     }
 }
--- src/main/java/GUI/Overview/OverviewController.java
+++ src/main/java/GUI/Overview/OverviewController.java
@@ -15,6 +15,7 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -23,12 +24,10 @@ public class OverviewController implements Initializable {
 
     private DashboardModel model;
 
-    @FXML private URL url;
-    @FXML private ResourceBundle resourceBundle;
     @FXML private TitledPane overviewTab;
     @FXML private TitledPane audienceSegmentsTab;
     @FXML private TitledPane contextTab;
-    @FXML private TitledPane ConfigurationTab;
+    @FXML private TitledPane configurationTab;
     @FXML private TitledPane campaignsTab;
     @FXML private TextField currentDate;
     @FXML private Text costPerClickText;
@@ -36,43 +35,38 @@ public class OverviewController implements Initializable {
     @FXML private Text costPerThousandImpressionsText;
     @FXML private Text totalClicksText;
     @FXML private PieChart overviewPieChart;
-    @FXML private ListView recommendationsListView;
-
-    public OverviewController() {
-        initialiseOverview();
-    }
+    @FXML private ListView<String> recommendationsListView;
 
     public void overviewButtonClicked(javafx.event.ActionEvent actionEvent) throws IOException {
         GUI.Main.changeScene("/fxml/Overview.fxml");
     }
 
-    public void initModel(DashboardModel model) {
-        if (this.model != null) {
-            throw new IllegalStateException("Model can only be initialised once");
-        }
+    public void setModel(DashboardModel model) {
         this.model = model;
     }
 
     public void initialiseOverview() {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/mm/dd");
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
         currentDate.setText(dateFormat.format(new Date()));
-        costPerClickText.setText(String.valueOf(this.model.getMetrics().getCostPerClick()));
-        totalImpressionsText.setText(String.valueOf(this.model.getMetrics().getNumOfImpressions()));
-        costPerThousandImpressionsText.setText(String.valueOf(this.model.getMetrics().getCostPerThousand()));
-        totalClicksText.setText(String.valueOf(this.model.getMetrics().getNumOfClicks()));
-        recommendationsListView = new ListView<String>(model.getRecommendations());
+        costPerClickText.setText("£" + decimalFormat.format(model.getMetrics().getCostPerClick()));
+        totalImpressionsText.setText(String.valueOf(model.getMetrics().getNumOfImpressions()));
+        costPerThousandImpressionsText.setText("£" + decimalFormat.format(model.getMetrics().getCostPerThousand()));
+        totalClicksText.setText(String.valueOf(model.getMetrics().getNumOfClicks()));
+        recommendationsListView.setItems(model.getRecommendations());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        assert overviewTab != null : "fx:id=\"overviewButton\" was not injected: check your FXML file 'overview.fxml'.";
-        assert contextTab != null : "fx:id=\"contextAccordian\" was not injected: check your FXML file 'overview.fxml'.";
-        assert audienceSegmentsTab != null : "fx:id=\"audienceSegmentsAccordian\" was not injected: check your FXML file 'overview.fxml'.";
-        assert campaignsTab != null : "fx:id=\"campaignAccordian\" was not injected: check your FXML file 'overview.fxml'.";
-        assert costPerClickText != null : "fx:id=\"numericalValueCPC\" was not injected: check your FXML file 'overview.fxml'.";
-        assert totalImpressionsText != null : "fx:id=\"numericalValueOfImpressions\" was not injected: check your FXML file 'overview.fxml'.";
-        assert costPerThousandImpressionsText != null : "fx:id=\"numericalValueCPM\" was not injected: check your FXML file 'overview.fxml'.";
-        assert totalClicksText != null : "fx:id=\"numericalValueNumOfClicks\" was not injected: check your FXML file 'overview.fxml'.";
+        assert overviewTab != null : "fx:id=\"overviewTab\" was not injected: check your FXML file 'overview.fxml'.";
+        assert contextTab != null : "fx:id=\"contextTab\" was not injected: check your FXML file 'overview.fxml'.";
+        assert audienceSegmentsTab != null : "fx:id=\"audienceSegmentsTab\" was not injected: check your FXML file 'overview.fxml'.";
+        assert campaignsTab != null : "fx:id=\"campaignsTab\" was not injected: check your FXML file 'overview.fxml'.";
+        assert configurationTab != null : "fx:id=\"configurationsTab\" was not injected: check your FXML file 'overview.fxml'.";
+        assert costPerClickText != null : "fx:id=\"costPerClickText\" was not injected: check your FXML file 'overview.fxml'.";
+        assert totalImpressionsText != null : "fx:id=\"totalImpressionsText\" was not injected: check your FXML file 'overview.fxml'.";
+        assert costPerThousandImpressionsText != null : "fx:id=\"costPerThousandImpressionsText\" was not injected: check your FXML file 'overview.fxml'.";
+        assert totalClicksText != null : "fx:id=\"totalClicksText\" was not injected: check your FXML file 'overview.fxml'.";
         assert overviewPieChart != null : "fx:id=\"overviewPieChart\" was not injected: check your FXML file 'overview.fxml'.";
         assert currentDate != null : "fx:id-\"currentDate\" was not injected: check your FXML file 'overview.fxml'.";
         costPerClickText.setText("0");
